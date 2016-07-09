122e827cd5cf9767ec337b4c2bfdfd130c8ca725
Adriana Black <adrianablancov@gmail.com>
2016-07-09 07:52:28 +0100

    assigned all keys to change classes but for the up key

--- memorygame.js
+++ memorygame.js
@@ -6,15 +6,53 @@
 //   })
 
 
-  // var tile = document.getElementsByClassName("player");
+  
 var squares = $('li');
 squares.first().addClass('player');
 
-function listenForMove(){
-   $('.neutral').on('click', function(){
 
-   $(squares).removeClass('player');
-   $(this).addClass('player');
+var currentPosition = 0;
+
+function listenForMove(){
+   $('body').on('keydown', function(e){
+    // e.preventDefault();
+
+
+    //on keydown DOWN
+    if(e.which === 40){
+      currentPosition += 5;
+      $(squares).removeClass('player');
+      $(squares[currentPosition]).addClass('player');
+      console.log("square", currentPosition);
+    } 
+//  MOVE RIGHT
+    else if (e.which === 39){
+      currentPosition += 1;
+      $(squares).removeClass('player');
+      $(squares[currentPosition]).addClass('player');
+      console.log("square", currentPosition);
+    } 
+// MOVE LEFT
+    else if (e.which === 37){
+      currentPosition += -1;
+      $(squares).removeClass('player');
+      $(squares[currentPosition]).addClass('player');
+      console.log("square", currentPosition);
+
+    }
+
+// MOVE UP key code ==38 - can not set condition for moving up when starting from 0
+
+    else if (e.which === 38){
+      currentPosition += -1;
+      $(squares).removeClass('player');
+      $(squares[currentPosition]).addClass('player');
+      console.log("square", currentPosition);
+
+    }
+
+
+
   })
 
 }
@@ -22,10 +60,7 @@ function listenForMove(){
 listenForMove();
 
 
-   var currentPosition = 0;
-
-
-// console.log($('.neutral'));
+   
 
 
 
